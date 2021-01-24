dbb17b85a8a379f3b823afc9f4e8e3c516698181
gshau <gshaughnessy@me.com>
2021-01-24 16:05:36 -0600

    new logs on start

--- src/astro_planner/logger.py
+++ src/astro_planner/logger.py
@@ -5,7 +5,7 @@ import os
 
 
 handler = logging.handlers.WatchedFileHandler(
-    os.environ.get("LOGFILE", "/logs/planner.log")
+    os.environ.get("LOGFILE", "/logs/planner.log"), mode="w"
 )
 formatter = logging.Formatter(fmt="%(asctime)s %(module)s %(message)s")
 handler.setFormatter(formatter)

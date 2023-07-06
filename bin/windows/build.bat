dd506437c02950e421df3520aadc8e48e1f8fa97
Gabe Shaughnessy <gshaughnessy@me.com>
2023-07-06 10:14:52 -0500

    update paths

--- bin/windows/build.bat
+++ bin/windows/build.bat
@@ -2,7 +2,7 @@
 git log -n 1 --pretty=format:"%%H" > git.hash
 .\venv\Scripts\create-version-file.exe .\metadata.yml --outfile file_version_info.txt
 
-.\venv\Scripts\pyinstaller.exe --distpath .\dist --noconfirm main-windows.spec
+.\venv\Scripts\pyinstaller.exe --distpath .\dist --noconfirm .\build_assets\windows\main.spec
 
-"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" .\aip_new.iss
-"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" .\aip_update.iss
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" .\build_assets\windows\aip_new.iss
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" .\build_assets\windows\aip_update.iss
